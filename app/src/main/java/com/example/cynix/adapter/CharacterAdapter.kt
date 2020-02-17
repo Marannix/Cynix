b753bd0b16aa323d9a1cd5da83b01e8c5b5ff13e
Joshua Onabanjo <onabanjotobi@gmail.com>
2020-02-17 18:22:45 +0000

    Created an adapter and populated the view

---
+++ app/src/main/java/com/example/cynix/adapter/CharacterAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.cynix.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cynix.R
+import com.example.cynix.character.Character
+import com.example.cynix.remote.CharacterApiContract
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_characters.view.*
+
+class CharacterAdapter : RecyclerView.Adapter<CharacterAdapter.ViewHolder>() {
+
+    private var data: List<Character> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_characters, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (data.isNotEmpty()) {
+            holder.bind(data[position])
+        }
+    }
+
+    fun setData(charactersResults: List<Character>) {
+        data = charactersResults
+        this.notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(charactersResults: Character) {
+            itemView.characterId.text = "#${charactersResults.id}"
+            itemView.characterName.text = charactersResults.name
+            itemView.characterStatus.text = charactersResults.status
+            itemView.characterSpecies.text = charactersResults.species
+            itemView.characterLocation.text = charactersResults.location.name
+
+            Picasso.get().load(charactersResults.image).into(itemView.characterImage)
+
+        }
+    }
+}
--- app/src/main/java/com/example/cynix/fragment/CharacterFragment.kt
+++ app/src/main/java/com/example/cynix/fragment/CharacterFragment.kt
@@ -8,11 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cynix.R
+import com.example.cynix.adapter.CharacterAdapter
 import com.example.cynix.dialog.FullscreenLoadingDialog
 import com.example.cynix.viewmodel.CharactersViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.fragment_character.*
 
 class CharacterFragment : BaseFragment() {
 
@@ -22,9 +25,12 @@ class CharacterFragment : BaseFragment() {
     private fun Disposable.addDisposable() = disposables.add(this)
     private val disposables = CompositeDisposable()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val charactersAdapter = CharacterAdapter()
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         stuff()
+        setAdapter()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -60,6 +66,7 @@ class CharacterFragment : BaseFragment() {
                 Log.d("loading", state.isLoading.toString())
 
                 state.listOfCharacters.let {
+                    charactersAdapter.setData(it)
                     Log.d("Characters ->", state.listOfCharacters.size.toString())
                 }
 
@@ -71,6 +78,11 @@ class CharacterFragment : BaseFragment() {
                 }
 
             }.addDisposable()
+    }
+
+    private fun setAdapter() {
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = charactersAdapter
     }
 
      interface OnFragmentInteractionListener {
